2c671d735eea458a6ab0315927fb860ce7d4e368
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-19 21:54:24 +0100

    validate user?

---
+++ public/netlify/functions/validate-user/validate-user.js
@@ -0,0 +1,26 @@
+// My thanks to https://hasura.io/blog/custom-auth-with-netlify/
+
+const fetch = require("node-fetch");
+
+exports.handler = async (event, context) => {
+  const {
+    identity,
+    user
+  } = context.clientContext;
+  if (user) {
+    const userID = user.sub;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        "X-Hasura-User-Id": userID,
+        "X-Hasura-Role": "user"
+      })
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      "X-Hasura-role": "anonymous"
+    })
+  };
+};
