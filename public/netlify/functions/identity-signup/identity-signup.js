9ab153acbb9738749163c1afeec0c68c9b0f5b71
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-19 21:36:31 +0100

    setup hasura user logging?

--- public/netlify/functions/identity-signup/identity-signup.js
+++ public/netlify/functions/identity-signup/identity-signup.js
@@ -4,26 +4,55 @@
 // more:
 // https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
 // https://www.netlify.com/docs/functions/#identity-and-functions
+//
+// My thanks to: https://hasura.io/blog/custom-auth-with-netlify/
 
-const handler = async function (event) {
-  const data = JSON.parse(event.body)
-  const { user } = data
+const fetch = require("node-fetch");
 
-  const responseBody = {
-    app_metadata: {
-      roles: user.email.split('@')[1] === 'trust-this-company.com' ? ['editor'] : ['visitor'],
-      my_user_info: 'this is some user info',
-    },
-    user_metadata: {
-      // append current user metadata
-      ...user.user_metadata,
-      custom_data_from_function: 'hurray this is some extra metadata',
-    },
+exports.handler = async function(event, context) {
+  const { user } = JSON.parse(event.body);
+
+  const responseBodyString = JSON.stringify({
+    query: `
+    mutation insertUser($id: String, $email:String, $name:String){
+      insert_users(objects: {id: $id, email: $email, name: $name}) {
+        affected_rows
+      }
+    }    
+  `,
+    variables: {
+      id: user.id,
+      email: user.email,
+      name: user.user_metadata.full_name
+    }
+  });
+
+  console.log(responseBodyString);
+
+  const result = await fetch(
+    "https://shopkeep-and-heroes.hasura.app/v1/graphql",
+    {
+      method: "POST",
+      body: responseBodyString,
+      headers: {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret":
+          process.env.HASURA_SECRET
+      }
+    }
+  );
+  const { errors, data } = await result.json();
+
+  if (errors) {
+    console.log(errors);
+    return {
+      statusCode: 500,
+      body: "Something is wrong"
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(responseBody)
+    };
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(responseBody),
-  }
-}
-
-module.exports = { handler }
+};
