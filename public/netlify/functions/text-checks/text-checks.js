8273e4151b6e81a6fcc003739d7aef5882343573
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-25 22:29:54 +0100

    set up checks in serverless, no submission to db

--- public/netlify/functions/text-checks/text-checks.js
+++ public/netlify/functions/text-checks/text-checks.js
@@ -1,17 +1,43 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
-  try {
+    try {
 
-    
+        let check = true;
+        let item_name = document.getElementById("itemInput").value;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Submitted` }),
+        if (item_name == "") {
+            check = false;
+        }
+
+        let banned_words = ["anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow job","bollock","bollok","boner","boob","bugger","bum","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","f u c k","fudgepacker","fudge packer","flange","Goddamn","God damn","jerk","jizz","knobend","knob end","labia","lmao","lmfao","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","scrotum","shit","s hit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore","wtf"];
+        for (let i = 0; i < banned_words.length; i++) {
+            if (item_name.includes(banned_words[i])) {
+                check = false;
+            }
+        } 
+
+        // allowed characters
+        // eslint-disable-next-line no-useless-escape
+        if (!(new RegExp("[a-z,A-Z,0-9,\?,\!,\.,\-,\*,\+,', ]+$").test(item_name))) {
+            check = false;
+        }
+
+        if (check == false) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify( { message: "The item failed one or more checks" } )
+            }
+        }
+
+        return {
+            statusCode: 202,
+            body: JSON.stringify({ message: "Submitted" }),
+        }
+
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
+// eslint-disable-next-line no-undef
 module.exports = { handler }
