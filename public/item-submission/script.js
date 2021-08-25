341c132d394bd384ee6425a772be7741753b455f
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-25 21:49:22 +0100

    much work in a few places, and add eslint

--- public/item-submission/script.js
+++ public/item-submission/script.js
@@ -1,6 +1,52 @@
+// eslint-disable-next-line no-unused-vars
 function onSubmitItem() {
 
     // Submit the item to the database
     
 
 }
+
+// Get some items in onload
+async function browseItems(page) {
+
+    const response = await fetch('https://shopkeep-and-heroes.hasura.app/api/rest/items/createJourney/' + page);
+    const parsed_json = await response.json(); //extract JSON from the http response
+    console.log(parsed_json);
+
+    for (let i=0; i < parsed_json.length; i++) {
+        continue;
+    }
+
+}
+
+// function to interpret the timestamps into something readable
+function parseTimeStamp(time) {
+    return time.slice(8, 10) + "/" + time.slice(5, 7) + "/" + time.slice(0, 4) + " @ " + time.slice(11, 16);
+}
+
+window.onload = function() {
+    
+    browseItems(0);
+
+}
+
+// Check the text for swears and stuff we don't want
+function include(file) {
+  
+    var script  = document.createElement('script');
+    script.src  = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+    
+    document.getElementsByTagName('head').item(0).appendChild(script);
+    
+}
+include("../text-checks-live.js");
+
+window.setInterval( () => {
+
+    // eslint-disable-next-line no-undef
+    let result = checktext(document.getElementById("itemInput").value);
+    document.getElementById("itemTitle").innerText = result;
+
+}, 1000)
---
+++ public/text-checks-live.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-useless-escape */
+// eslint-disable-next-line no-unused-vars
+function checktext(item_name) {
+
+    // A note to slightly malicious/curious users editing this js:
+    // The checks performed here are only to give live and more useful feedback to the user, proper checks are performed after hitting submit.
+
+    let default_prompt = "Fill in the prompt below with the name of your item!"
+    item_name = item_name.trim()
+
+    if (item_name == "") {
+        return default_prompt;
+    }
+
+    let banned_words = ["anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow job","bollock","bollok","boner","boob","bugger","bum","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","f u c k","fudgepacker","fudge packer","flange","Goddamn","God damn","jerk","jizz","knobend","knob end","labia","lmao","lmfao","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","scrotum","shit","s hit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore","wtf"];
+    for (let i = 0; i < banned_words.length; i++) {
+        if (item_name.includes(banned_words[i])) {
+            return "Your item contains banned words (" + banned_words[i] + ") ";
+        }
+    } 
+
+    // allowed characters
+    if (!(new RegExp("[a-z,A-Z,0-9,\?,\!,\.,\-,\*,\+,', ]+$").test(item_name))) {
+        return "Your item contains disallowed characters!";
+    }
+
+    return default_prompt;
+}
