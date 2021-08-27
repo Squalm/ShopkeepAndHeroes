258c5d1c1cf5597a3efeb2d32b1e926b4dbeb386
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-27 13:31:43 +0100

    hopefully actually submit items now?

--- public/item-submission/script.js
+++ public/item-submission/script.js
@@ -2,17 +2,10 @@
 async function onSubmitItem() {
 
     // Submit the item to the database by invoking the function
-    const request_url = "https://shopkeepandheroes.netlify.app/.netlify/functions/text-checks?iname=" + document.getElementById("itemInput").value;
-    var xhr = new XMLHttpRequest();
-    let response = ""
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.response);
-            response = xhr.responseText;
-        }
-    }
-    xhr.open("GET", request_url, true);
-    document.getElementById("itemTitle").innerText = response;
+    const request_url = "../.netlify/functions/text_checks/text_checks?iname=" + document.getElementById("itemInput").value;
+    document.getElementById("itemTitle").value = fetch(request_url).then((response) => {
+        return response.json()
+    });
 
 }
 
