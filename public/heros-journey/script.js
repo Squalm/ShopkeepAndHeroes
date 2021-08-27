4db3c50b40066bd42005e4c74d16345bb22f8e81
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-27 13:49:58 +0100

    move browseitems

---
+++ public/heros-journey/script.js
@@ -0,0 +1,16 @@
+// Get some items in onload
+async function browseItems(page) {
+
+    const response = await fetch('https://shopkeep-and-heroes.hasura.app/api/rest/items/createJourney/' + page);
+    const parsed_json = await response.json(); //extract JSON from the http response
+    console.log(parsed_json);
+
+    for (let i=0; i < parsed_json.length; i++) {
+        continue;
+    }
+
+}
+
+window.onload( () => {
+    browseItems(0);
+})
--- public/item-submission/script.js
+++ public/item-submission/script.js
@@ -12,20 +12,6 @@ async function onSubmitItem() {
 
 }
 
-// Get some items in onload
-// Move this to heros-journey soon
-async function browseItems(page) {
-
-    const response = await fetch('https://shopkeep-and-heroes.hasura.app/api/rest/items/createJourney/' + page);
-    const parsed_json = await response.json(); //extract JSON from the http response
-    console.log(parsed_json);
-
-    for (let i=0; i < parsed_json.length; i++) {
-        continue;
-    }
-
-}
-
 // Check the text for swears and stuff we don't want
 function include(file) {
   
