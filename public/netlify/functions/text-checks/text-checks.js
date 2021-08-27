7cafb5414daa40d2288501a1fdf695347fc15340
Squalm <43246103+Squalm@users.noreply.github.com>
2021-08-27 23:54:45 +0100

    change phrasing? to get user info?

--- public/netlify/functions/text-checks/text-checks.js
+++ public/netlify/functions/text-checks/text-checks.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 const { response } = require("express");
 
-const handler = async (event, context) => {
+exports.handler = async (event, context) => {
     try {
 
         let check = true;
